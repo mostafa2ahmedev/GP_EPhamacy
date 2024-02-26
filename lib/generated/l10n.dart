1492913494e7485e5b58845b240c429e42f6ea6e
mostafa2ahmedev <mostafa2002ahmedev@gmail.com>
2024-02-26 22:05:52 +0200

    updated

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -79,91 +79,12 @@ class S {
       args: [],
     );
   }
-  /// add account
-  String get AddAccount {
-    return Intl.message(
-      'Add Account',
-      name: 'AddAccount',
-      desc: '',
-      args: [],
-    );
-  }
 
   /// `Forgot Passowrd ?`
   String get AuthForgotPassword {
     return Intl.message(
       'Forgot Passowrd ?',
       name: 'AuthForgotPassword',
-      desc: '',
-      args: [],
-    );
-  }
-
-  /// `D A S H B O A R D`
-  String get HomeDashboard {
-    return Intl.message(
-      'D A S H B O A R D',
-      name: 'HomeDashboard',
-      desc: '',
-      args: [],
-    );
-  }
-
-  /// `S T O R E S `
-  String get HomeStores {
-    return Intl.message(
-      'S T O R E S ',
-      name: 'HomeStores',
-      desc: '',
-      args: [],
-    );
-  }
-
-  /// `P A T I E N T S`
-  String get HomePatients {
-    return Intl.message(
-      'P A T I E N T S',
-      name: 'HomePatients',
-      desc: '',
-      args: [],
-    );
-  }
-
-  /// `A D D A C C O U N T`
-  String get AddAccount {
-    return Intl.message(
-      'A D D A C C O U N T',
-      name: 'AddAccount',
-      desc: '',
-      args: [],
-    );
-  }
-
-  /// `Log out`
-  String get Resgistarion {
-    return Intl.message(
-      'Log out',
-      name: 'Resgistarion',
-      desc: '',
-      args: [],
-    );
-  }
-
-  /// `Student Phamacy`
-  String get SidalyaEltlba {
-    return Intl.message(
-      'Student Phamacy',
-      name: 'SidalyaEltlba',
-      desc: '',
-      args: [],
-    );
-  }
-
-  /// `Mode`
-  String get Mode {
-    return Intl.message(
-      'Mode',
-      name: 'Mode',
       desc: '',
       args: [],
     );
